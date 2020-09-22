a936e7695c64f3f3d707b78d9d74dfa164af4bd2
Miguel <miguel.espinoza.silloca@gmail.com>
2020-09-22 18:12:03 -0500

    Creating a route to Delete Course

--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,8 @@ const {
     getCourses,
     getCourse,
     addCourse,
-    updateCourse
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses')
 
 const router = express.Router({mergeParams:true})
@@ -17,5 +18,6 @@ router
     .route('/:id')
     .get(getCourse)
     .put(updateCourse)
+    .delete(deleteCourse)
 
 module.exports = router
