b9fb098eced9eff31daaf23105564f1b4e77e3d7
Miguel <miguel.espinoza.silloca@gmail.com>
2020-09-29 13:17:48 -0500

    Creating a method to check if the password matches correctly

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 
 // @desc            Register user
-// @route           GET /api/v1/auth/register
+// @route           POST /api/v1/auth/register
 // @access          Public
 exports.register = asyncHandler(async (req, res, next)=>{
     const { name, email, password, role}= req.body
@@ -19,13 +19,42 @@ exports.register = asyncHandler(async (req, res, next)=>{
     // Create a token
     const token = user.getSignedJwtToken()
 
+    res.status(200).json({
+        success:true,
+        token
+    })
+})
+
+// @desc            Login user
+// @route           POST /api/v1/auth/login
+// @access          Public
+exports.login = asyncHandler(async (req, res, next)=>{
+    const {email, password}= req.body
+
+    //Validate email & password
+    if (!email||!password){
+        return next(new ErrorResponse('Please provide an email and password',400))
+    }
+
+    //Check for a user
+    const user = await user.findOne({email}).select('+password')
+    if (!user){
+        return next(new ErrorResponse('Invalid credentials',401))
+    }
+
+    //Check if password matches
+    const isMatch = await user.matchPassword(password)
+    if (!isMatch){
+        return next(new ErrorResponse('Invalid credentials',401))
+    }
+
+
+
+    // Create a token
+    const token = user.getSignedJwtToken()
 
     res.status(200).json({
         success:true,
         token
     })
-
-
-
-
 })
