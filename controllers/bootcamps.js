1c78e770c3151e7341afb0436cb1025eda370fe7
Miguel <miguel.espinoza.silloca@gmail.com>
2020-09-30 01:33:41 -0500

    Adding the user and checking that the publisher just add one bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,8 +31,19 @@ exports.getBootcamp = asyncHandler(async (req,res,next)=>{
 // @access          Private
 
 exports.createBootcamp = asyncHandler(async (req,res,next)=>{
-        const bootcamp = await Bootcamp.create(req.body);
-        res.status(201).json({
+    //TODO: Add user to body.user
+    req.body.user = req.user
+
+    //TODO: Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({user:req.user.id})
+
+    //TODO: If the user is not and admin, they can only add one bootcamp
+    if (publishedBootcamp && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`,400))
+    }
+
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
             success : true,
             data : bootcamp
         })
--- middleware/auth.js
+++ middleware/auth.js
@@ -38,6 +38,5 @@ exports.authorize = (...roles)=>{
         }
         next()
     }
-
 }
 
