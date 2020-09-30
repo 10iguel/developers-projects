5798569c1e17a486e138c43096897a990652f4d9
Miguel <miguel.espinoza.silloca@gmail.com>
2020-09-30 00:42:01 -0500

    Using the function to get token ,create a cookie and send response

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,13 +16,7 @@ exports.register = asyncHandler(async (req, res, next)=>{
         role
     })
 
-    // Create a token
-    const token = user.getSignedJwtToken()
-
-    res.status(200).json({
-        success:true,
-        token
-    })
+    sendTokenResponse(user,200,res)
 })
 
 // @desc            Login user
@@ -37,7 +31,7 @@ exports.login = asyncHandler(async (req, res, next)=>{
     }
 
     //Check for a user
-    const user = await user.findOne({email}).select('+password')
+    const user = await User.findOne({email}).select('+password')
     if (!user){
         return next(new ErrorResponse('Invalid credentials',401))
     }
@@ -48,13 +42,42 @@ exports.login = asyncHandler(async (req, res, next)=>{
         return next(new ErrorResponse('Invalid credentials',401))
     }
 
+    sendTokenResponse(user,200,res)
+})
 
-
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user,statusCode,res)=>{
     // Create a token
     const token = user.getSignedJwtToken()
 
-    res.status(200).json({
-        success:true,
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === 'production'){
+        options.secure = true
+    }
+
+    res
+        .status(statusCode)
+        .cookie('token',token,options)
+        .json({
+        success: true,
         token
     })
+}
+
+// @desc            Get current logged in user
+// @route           POST /api/v1/auth/me
+// @access          Private
+
+exports.getMe = asyncHandler(async (req,res,next)=>{
+    const user = await User.findById(req.user.id)
+
+    res.status(200).json({
+        success : true,
+        data : user
+    })
+
 })
