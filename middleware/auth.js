fdf1070029a714ab5405a128b81cad8b07b49f6b
Miguel <miguel.espinoza.silloca@gmail.com>
2020-10-06 08:53:00 -0500

    Clearer the token , and creating logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,18 +7,21 @@ const User = require('../models/User')
 
 exports.protect = asyncHandler(async (req,res,next)=>{
     let token
+
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        //Set token from Bearer token in headers
         token = req.headers.authorization.split(' ')[1]
+        //Set token from cookie
     }
-   // else if (req.cookie.token){
-      //  token = req.cookie.token
+    //else if (req.cookies.token){
+        //Set token from cookie
+        //token = req.cookies.token
     //}
 
     // Make sure token exists
     if (!token){
         return next(new ErrorResponse('Not authorize to access this route', 401))
     }
-
     try {
         //verify token
         const decoded = jwt.verify(token,process.env.jwt_secret)
